5e4f4312c21a291c65a61c408dbd73b640bd111b
Cody Tomesch <tomeschcody@gmail.com>
2018-11-12 11:18:39 -0700

    all games in one with selector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
+import Tetris from './components/Tetris'
+import Snake from './components/Snake'
+import MineSweeper from './components/Minesweeper'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      game: 'Tetris'
+    }
+  }
+
+  handleGameChange(game) {
+    this.setState({game})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        <select onChange={e => this.handleGameChange(e.target.value)}>
+          <option value={'Tetris'} >Tetirs</option>
+          <option value={'Snake'} >Snake</option>
+          <option value={'MineSweeper'} >Minesweeper</option>
+        </select>
+        {this.state.game === 'Tetris' && <Tetris />}
+        {this.state.game === 'Snake' && <Snake />}
+        {this.state.game === 'MineSweeper' && <MineSweeper />}
       </div>
     );
   }
